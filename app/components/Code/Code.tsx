42eb76dc757cb1321b8f6111bd6920dda8de2401
kosei <abdelhamid.boudjit@gmail.com>
2023-07-29 22:14:18 +0100

    0.8.1

--- app/components/Code/Code.tsx
+++ app/components/Code/Code.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { Code as SyntaxHighlighter } from "bright";
+
 interface Props {
-  code: string;
-  language: string;
+  value: {
+    code: string;
+    language: string;
+  };
 }
 const Code: React.FC<Props> = (props) => {
-  const { code, language } = props;
-  return (
-    <SyntaxHighlighter language={language} style={dark}>
-      {code}
-    </SyntaxHighlighter>
-  );
+  const { code, language } = props.value;
+  return <SyntaxHighlighter lang={language}>{code}</SyntaxHighlighter>;
 };
 
 export default Code;

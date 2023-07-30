515b97186e474c222bbb4548c54c0d569c086eba
kosei <abdelhamid.boudjit@gmail.com>
2023-07-30 21:19:43 +0100

    0.9.4

--- app/components/SlideMenu/SlideMenu.tsx
+++ app/components/SlideMenu/SlideMenu.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { size, stroke } from "@/constants/elements/icons";
 import { IconMenu2, IconX } from "@tabler/icons-react";
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 
 function SlideMenu({
   children,
@@ -18,7 +18,7 @@ function SlideMenu({
       <IconMenu2 className="cursor-pointer" onClick={() => setShow(true)} />
       <div
         className={`${
-          show ? "rounded-none scale-100 " : "scale-0"
+          show ? "backdrop-blur rounded-none scale-100 " : "scale-0"
         } origin-top-right absolute p-4 top-0 left-0 w-full h-screen bg-white dark:bg-slate-900`}
       >
         <div className="py-2 flex items-center justify-between">
@@ -30,7 +30,7 @@ function SlideMenu({
             onClick={() => setShow(false)}
           />
         </div>
-        {children}
+        <div onClick={() => setShow(false)}>{children}</div>
       </div>
     </div>
   );

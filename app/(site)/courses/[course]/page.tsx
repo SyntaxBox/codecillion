521452998856f02b97efe1c242315f5a77e9f8b9
kosei <abdelhamid.boudjit@gmail.com>
2023-07-30 14:52:18 +0100

    0.9.1

--- app/(site)/courses/[course]/page.tsx
+++ app/(site)/courses/[course]/page.tsx
@@ -1,12 +1,5 @@
-"use client";
 import ComingSoon from "@/app/components/ComingSoon/ComingSoon";
-import { courseMetadata } from "@/data/meta/pages";
-import { Metadata } from "next";
 import React from "react";
-
-export const metadata: Metadata = {
-  title: "hello world",
-};
 
 export default function page() {
   return (

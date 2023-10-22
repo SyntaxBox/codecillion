207579947a3f39e16c0d9a0e7ead342e677229dc
kosei <abdelhamid.boudjit@gmail.com>
2023-10-22 23:06:04 +0100

    Update icons.ts

--- data/meta/global/icons.ts
+++ data/meta/global/icons.ts
@@ -1,17 +1,18 @@
+import { URL } from "@/constants/other";
 import { Metadata } from "next";
 export const iconsMetadata: Metadata = {
   icons: {
     icon: [
-      "icons/icon-512x512.png",
-      "icons/icon-384x384.png",
-      "icons/icon-192x192.png",
-      "icons/icon-128x128.png",
+      `${URL}/icons/icon-512x512.png`,
+      `${URL}/icons/icon-384x384.png`,
+      `${URL}/icons/icon-192x192.png`,
+      `${URL}/icons/icon-128x128.png`,
     ],
     apple: [
-      "icons/icon-512x512.png",
-      "icons/icon-384x384.png",
-      "icons/icon-192x192.png",
-      "icons/icon-128x128.png",
+      `${URL}/icons/icon-512x512.png`,
+      `${URL}/icons/icon-384x384.png`,
+      `${URL}/icons/icon-192x192.png`,
+      `${URL}/icons/icon-128x128.png`,
     ],
   },
 };

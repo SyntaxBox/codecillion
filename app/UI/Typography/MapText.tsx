787bb9b5052620a76305de822e0d0893678221ed
kosei <abdelhamid.boudjit@gmail.com>
2023-10-23 23:39:20 +0100

    Update MapText.tsx

--- app/UI/Typography/MapText.tsx
+++ app/UI/Typography/MapText.tsx
@@ -1,4 +1,5 @@
 import { CourseMapType } from "@/interfaces/queries";
+import Link from "next/link";
 import React from "react";
 
 function MapText({
@@ -10,7 +11,21 @@ function MapText({
   type: CourseMapType;
   to: string;
 }) {
-  return <div>MapText</div>;
+  return (
+    <>
+      {type === "module" && (
+        <h2 className="text-2xl font-bold dark:text-emerald-200 text-emerald-700">
+          {children}
+        </h2>
+      )}
+      {type === "chapter" && (
+        <h3 className="text-xl font-black dark:text-slate-300 text-slate-600">
+          {children}
+        </h3>
+      )}
+      {type === "courseLesson" && <Link href={to}>{children}</Link>}
+    </>
+  );
 }
 
 export default MapText;

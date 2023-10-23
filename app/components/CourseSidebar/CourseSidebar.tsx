5f62d26a60dc2a88220340fb4cef62026113a855
kosei <abdelhamid.boudjit@gmail.com>
2023-10-23 23:39:27 +0100

    Update CourseSidebar.tsx

--- app/components/CourseSidebar/CourseSidebar.tsx
+++ app/components/CourseSidebar/CourseSidebar.tsx
@@ -1,13 +1,19 @@
+import Accordion from "@/app/UI/Typography/Accordion";
+import MapText from "@/app/UI/Typography/MapText";
 import { CourseContentMap } from "@/interfaces/queries";
 import React from "react";
 
 function CourseSidebar({ mapContent }: { mapContent: CourseContentMap[] }) {
   return (
     <aside>
-      {mapContent.map(({ slug, title }, i) => {
-        console.log(slug);
-        return <p key={i}>{title}</p>;
+      {mapContent.map(({ slug, title, type }, i) => {
+        return (
+          <MapText key={i} to={`${slug}`} type={type}>
+            {title}
+          </MapText>
+        );
       })}
+      <Accordion />
     </aside>
   );
 }

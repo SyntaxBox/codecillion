16f3ec526fd41b475312a62be17d3ff36e10e321
kosei <abdelhamid.boudjit@gmail.com>
2023-10-24 22:10:14 +0100

    Update CourseSidebar.tsx

--- app/components/CourseSidebar/CourseSidebar.tsx
+++ app/components/CourseSidebar/CourseSidebar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Accordion from "@/app/UI/Typography/Accordion";
-import { CourseContentMap } from "@/interfaces/queries";
+import { CourseAccordion, CourseContentMap } from "@/interfaces/queries";
 import { transformToAccordion } from "@/logic/transform";
 import { getCourseContentBySlug } from "@/sanity/utils";
 import { useParams } from "next/navigation";
@@ -8,36 +8,43 @@ import React, { useEffect, useState } from "react";
 
 function CourseSidebar() {
   const params = useParams();
-  const [course, setCourse] = useState<CourseContentMap[]>([]);
-  console.log(params);
+  const [course, setCourse] = useState<CourseContentMap[] | null>(null);
+  const [courseAccordion, setCourseAccordion] = useState<
+    CourseAccordion[] | null
+  >(null);
+  const handleCourseContent = async () => {
+    if (!params?.slug || !params?.lesson) console.log("fatal at CourseSidebar");
+    else {
+      const courseContent = await getCourseContentBySlug(
+        Array.isArray(params.slug) ? params.slug[0] : params.slug
+      );
+      console.log("fuck", params.slug);
+      console.log(Array.isArray(params.slug) ? params.slug[0] : params.slug);
+      setCourse(courseContent.content);
+    }
+  };
   useEffect(() => {
-    async () => {
-      if (!params?.slug || !params?.lesson)
-        console.log("fatal at CourseSidebar");
-      else {
-        const course = await getCourseContentBySlug(
-          Array.isArray(params.slug) ? params.slug[0] : params.slug
-        );
-        setCourse(course);
-      }
-    };
-  });
+    handleCourseContent();
+  }, [params]);
+  useEffect(() => {
+    if (course) setCourseAccordion(transformToAccordion(course));
+  }, [course]);
   if (!params?.slug || !params?.lesson) return null;
-
   return (
     <aside>
-      {/* {accordion.map(({ title, links }, i) => {
-        return (
-          <Accordion
-            title={title}
-            links={links}
-            currentIndex={0}
-            key={i}
-            isFirst={i === 0}
-            isLast={i === accordion.length - 1}
-          />
-        );
-      })} */}
+      {courseAccordion &&
+        courseAccordion.map(({ title, links }, i) => {
+          return (
+            <Accordion
+              title={title}
+              links={links}
+              currentIndex={0}
+              key={i}
+              isFirst={i === 0}
+              isLast={i === courseAccordion.length - 1}
+            />
+          );
+        })}
     </aside>
   );
 }

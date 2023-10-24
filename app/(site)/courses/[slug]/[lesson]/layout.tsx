d692ffe11704353f2bd4ff673bfc5526587250cd
kosei <abdelhamid.boudjit@gmail.com>
2023-10-24 21:12:26 +0100

    Create layout.tsx

---
+++ app/(site)/courses/[slug]/[lesson]/layout.tsx
@@ -0,0 +1,55 @@
+// import { TWITTER } from "@/constants/other";
+// import {
+//   twitterMetadata,
+//   openGraphMetadata,
+//   themeColorMetadata,
+//   manifestMetadata,
+//   iconsMetadata,
+// } from "@/data/meta/global";
+// import { getCourseMetadataBySlug } from "@/sanity/utils";
+// import { Metadata } from "next";
+// import { metadata as dynamicMetadata } from "@/data/meta/pages/course";
+// import { URL } from "@/constants/other";
+
+import CourseSidebar from "@/app/components/CourseSidebar/CourseSidebar";
+import { ReactNode } from "react";
+
+type Props = {
+  params: { lesson: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+  router: any;
+};
+
+// export async function generateMetadata({ params }: Props): Promise<Metadata> {
+//   // read route params
+//   const { slug } = params;
+
+//   // fetch data
+//   const courseMetadata = await getCourseMetadataBySlug(slug);
+//   if (courseMetadata?.keywords === null) courseMetadata.keywords = [];
+//   if (courseMetadata) {
+//     const { title, description, thumbnail } = courseMetadata;
+//     const metadata: Metadata = {
+//       ...dynamicMetadata(courseMetadata),
+//       ...twitterMetadata(title, description, thumbnail, TWITTER),
+//       ...openGraphMetadata(title, description, thumbnail, `${URL}/${slug}`),
+//       ...themeColorMetadata,
+//       ...manifestMetadata,
+//       ...iconsMetadata,
+//     };
+//     return metadata;
+//   } else {
+//     return {
+//       title: "Not Found",
+//     };
+//   }
+// }
+
+export default function page({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <CourseSidebar />
+      {children}
+    </>
+  );
+}

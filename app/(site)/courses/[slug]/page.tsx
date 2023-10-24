244e1c418b37101725c31bc1f2637c6537669f56
kosei <abdelhamid.boudjit@gmail.com>
2023-10-24 21:12:37 +0100

    Update page.tsx

--- app/(site)/courses/[slug]/page.tsx
+++ app/(site)/courses/[slug]/page.tsx
@@ -1,9 +1,24 @@
+import ComingSoon from "@/app/components/ComingSoon/ComingSoon";
+import CourseSidebar from "@/app/components/CourseSidebar/CourseSidebar";
+import { transformToAccordion } from "@/logic/transform";
+import { getCourseContentBySlug } from "@/sanity/utils";
 import React from "react";
 type Props = {
   params: { slug: string };
   searchParams: { [key: string]: string | string[] | undefined };
 };
 
-export default function page({ params }: Props) {
-  return <>Hello world</>;
+const ComingCourse = (
+  <ComingSoon
+    description="We apologize for any inconvenience caused, but please note that the
+        recording and documentation of this section are currently in progress
+        and will be completed soon. Thank you for your understanding and
+        patience."
+  />
+);
+
+export default async function page({ params }: Props) {
+  const course = await getCourseContentBySlug(params.slug);
+  if (!course.content) return ComingCourse;
+  return <>Please Wait</>;
 }

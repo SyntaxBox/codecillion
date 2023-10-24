6a225a88c89237c64429770d004cddc811c5ba3e
kosei <abdelhamid.boudjit@gmail.com>
2023-10-24 21:12:48 +0100

    Update Accordion.tsx

--- app/UI/Typography/Accordion.tsx
+++ app/UI/Typography/Accordion.tsx
@@ -1,44 +1,52 @@
 "use client";
-import { IconArrowBadgeUpFilled } from "@tabler/icons-react";
+import {
+  IconArrowBadgeRightFilled,
+  IconArrowBadgeUpFilled,
+} from "@tabler/icons-react";
 import React, { useState } from "react";
 import A from "./A";
+import { CourseAccordion } from "@/interfaces/queries";
+import AccordionTitle from "./AccordionTitle";
 
-function Accordion() {
+function Accordion({
+  links,
+  title,
+  currentIndex,
+  isLast,
+  isFirst,
+}: CourseAccordion & {
+  currentIndex: number;
+  isLast: boolean;
+  isFirst: boolean;
+}) {
   const [show, setShow] = useState(false);
-  const isLast = true;
-  const isFirst = true;
-  const links: { title: string; slug: string }[] = [
-    {
-      title: "Introduction to HTML",
-      slug: "introduction",
-    },
-  ];
   return (
     <>
-      <h2>
+      <span>
         <button
           onClick={() => setShow(!show)}
           type="button"
-          className={`flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-gray-200  ${
+          className={`flex items-center justify-between w-full p-5 font-medium text-left text-slate-500 border border-slate-200  ${
             isLast && !show ? "rounded-b-xl" : ""
           }
           ${isFirst ? "rounded-t-xl" : ""}
           ${show ? "border-b-0" : ""}
-          focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800`}
+          focus:ring-4 focus:ring-slate-200 dark:focus:ring-slate-800 dark:border-slate-700 dark:text-slate-400 hover:bg-slate-100 dark:hover:bg-slate-800`}
         >
-          <span>What is Flowbite?</span>
+          <AccordionTitle>{title}</AccordionTitle>
           <IconArrowBadgeUpFilled className={`${show ? "rotate-180" : ""}`} />
         </button>
-      </h2>
-      <div className={`${show ? "scale-y-100 " : "scale-y-0"} transition-none`}>
+      </span>
+      <div className={`${show ? "scale-y-100 " : "hidden"}`}>
         <div
-          className={`p-5 border ${
+          className={`p-5 flex flex-col gap-1 border ${
             isLast ? "rounded-b-xl" : "border-b-0"
-          } border-gray-200 dark:border-gray-700 dark:bg-gray-900`}
+          } border-slate-200 dark:border-slate-700 dark:bg-slate-900`}
         >
           {links.map(({ title, slug }, i) => {
             return (
-              <A key={i} href={slug}>
+              <A className="flex gap-1" key={i} href={slug}>
+                {currentIndex === i && <IconArrowBadgeRightFilled />}
                 {title}
               </A>
             );

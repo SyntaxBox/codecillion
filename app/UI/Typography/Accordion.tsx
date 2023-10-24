b4dbcd0c253d6e40608eff29a088b5d9439872de
kosei <abdelhamid.boudjit@gmail.com>
2023-10-24 22:10:11 +0100

    Update Accordion.tsx

--- app/UI/Typography/Accordion.tsx
+++ app/UI/Typography/Accordion.tsx
@@ -7,6 +7,7 @@ import React, { useState } from "react";
 import A from "./A";
 import { CourseAccordion } from "@/interfaces/queries";
 import AccordionTitle from "./AccordionTitle";
+import { size } from "@/constants/elements/icons";
 
 function Accordion({
   links,
@@ -26,27 +27,24 @@ function Accordion({
         <button
           onClick={() => setShow(!show)}
           type="button"
-          className={`flex items-center justify-between w-full p-5 font-medium text-left text-slate-500 border border-slate-200  ${
-            isLast && !show ? "rounded-b-xl" : ""
-          }
-          ${isFirst ? "rounded-t-xl" : ""}
-          ${show ? "border-b-0" : ""}
-          focus:ring-4 focus:ring-slate-200 dark:focus:ring-slate-800 dark:border-slate-700 dark:text-slate-400 hover:bg-slate-100 dark:hover:bg-slate-800`}
+          className={`flex items-center w-full justify-between font-medium text-left text-slate-500
+          `}
         >
           <AccordionTitle>{title}</AccordionTitle>
           <IconArrowBadgeUpFilled className={`${show ? "rotate-180" : ""}`} />
         </button>
       </span>
       <div className={`${show ? "scale-y-100 " : "hidden"}`}>
-        <div
-          className={`p-5 flex flex-col gap-1 border ${
-            isLast ? "rounded-b-xl" : "border-b-0"
-          } border-slate-200 dark:border-slate-700 dark:bg-slate-900`}
-        >
+        <div className="p-2 flex items-start justify-start flex-col gap-1">
           {links.map(({ title, slug }, i) => {
             return (
-              <A className="flex gap-1" key={i} href={slug}>
-                {currentIndex === i && <IconArrowBadgeRightFilled />}
+              <A
+                className={`flex gap-1 items-center ${
+                  currentIndex === i ? "font-bold" : ""
+                }`}
+                key={i}
+                href={slug}
+              >
                 {title}
               </A>
             );

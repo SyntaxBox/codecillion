f98e85b7d5d7677ef5383e1a2e90155f7b80a347
kosei <abdelhamid.boudjit@gmail.com>
2023-10-25 23:56:42 +0100

    Update CourseSidebar.tsx

--- app/components/CourseSidebar/CourseSidebar.tsx
+++ app/components/CourseSidebar/CourseSidebar.tsx
@@ -18,8 +18,6 @@ function CourseSidebar() {
       const courseContent = await getCourseContentBySlug(
         Array.isArray(params.slug) ? params.slug[0] : params.slug
       );
-      console.log("fuck", params.slug);
-      console.log(Array.isArray(params.slug) ? params.slug[0] : params.slug);
       setCourse(courseContent.content);
     }
   };
@@ -31,22 +29,80 @@ function CourseSidebar() {
   }, [course]);
   if (!params?.slug || !params?.lesson) return null;
   return (
-    <aside className="w-72">
-      {courseAccordion &&
-        courseAccordion.map(({ title, links }, i) => {
-          return (
-            <Accordion
-              title={title}
-              links={links}
-              currentLesson={
-                Array.isArray(params.lesson) ? params.lesson[0] : params.lesson
-              }
-              key={i}
-              isFirst={i === 0}
-              isLast={i === courseAccordion.length - 1}
-            />
-          );
-        })}
+    <aside className="w-72 relative">
+      <div
+        style={{ height: "calc(100vh - 80px)" }}
+        className=" w-72 fixed overflow-scroll"
+      >
+        {courseAccordion &&
+          courseAccordion.map(({ title, links }, i) => {
+            return (
+              <Accordion
+                title={title}
+                links={links}
+                currentLesson={
+                  Array.isArray(params.lesson)
+                    ? params.lesson[0]
+                    : params.lesson
+                }
+                key={i}
+                isFirst={i === 0}
+                isLast={i === courseAccordion.length - 1}
+              />
+            );
+          })}
+        {courseAccordion &&
+          courseAccordion.map(({ title, links }, i) => {
+            return (
+              <Accordion
+                title={title}
+                links={links}
+                currentLesson={
+                  Array.isArray(params.lesson)
+                    ? params.lesson[0]
+                    : params.lesson
+                }
+                key={i}
+                isFirst={i === 0}
+                isLast={i === courseAccordion.length - 1}
+              />
+            );
+          })}
+        {courseAccordion &&
+          courseAccordion.map(({ title, links }, i) => {
+            return (
+              <Accordion
+                title={title}
+                links={links}
+                currentLesson={
+                  Array.isArray(params.lesson)
+                    ? params.lesson[0]
+                    : params.lesson
+                }
+                key={i}
+                isFirst={i === 0}
+                isLast={i === courseAccordion.length - 1}
+              />
+            );
+          })}
+        {courseAccordion &&
+          courseAccordion.map(({ title, links }, i) => {
+            return (
+              <Accordion
+                title={title}
+                links={links}
+                currentLesson={
+                  Array.isArray(params.lesson)
+                    ? params.lesson[0]
+                    : params.lesson
+                }
+                key={i}
+                isFirst={i === 0}
+                isLast={i === courseAccordion.length - 1}
+              />
+            );
+          })}
+      </div>
     </aside>
   );
 }

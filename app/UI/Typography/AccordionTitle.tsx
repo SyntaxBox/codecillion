dd3a4e3aec578095ac16d35ea94a31bd096f7dba
kosei <abdelhamid.boudjit@gmail.com>
2023-10-25 23:16:38 +0100

    Update AccordionTitle.tsx

--- app/UI/Typography/AccordionTitle.tsx
+++ app/UI/Typography/AccordionTitle.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react";
 
 function AccordionTitle({ children }: { children: ReactNode }) {
-  return <h2>{children}</h2>;
+  return <h2 className="font-bold">{children}</h2>;
 }
 
 export default AccordionTitle;

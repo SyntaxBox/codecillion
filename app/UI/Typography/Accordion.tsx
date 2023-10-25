3add7a91b5fe7387b9ba2fab5ea9857fe9ed8bcb
kosei <abdelhamid.boudjit@gmail.com>
2023-10-25 23:16:44 +0100

    Update Accordion.tsx

--- app/UI/Typography/Accordion.tsx
+++ app/UI/Typography/Accordion.tsx
@@ -2,13 +2,17 @@
 import {
   IconArrowBadgeRightFilled,
   IconArrowBadgeUpFilled,
+  IconChevronLeft,
+  IconChevronRight,
 } from "@tabler/icons-react";
 import React, { useState } from "react";
 import A from "./A";
 import { CourseAccordion } from "@/interfaces/queries";
 import AccordionTitle from "./AccordionTitle";
-import { size } from "@/constants/elements/icons";
+import { size, stroke } from "@/constants/elements/icons";
 import Link from "next/link";
+import AccordionLink from "./AccordionLink";
+import { findSlugIndex } from "@/logic/accordion";
 
 function Accordion({
   links,
@@ -19,39 +23,48 @@ function Accordion({
   isLast: boolean;
   isFirst: boolean;
 }) {
-  const currentLessonIndex = 0;
-  const currentChapter = true;
+  const currentLessonIndex = findSlugIndex(links, currentLesson);
+  const currentChapter = currentLessonIndex !== -1;
   const [show, setShow] = useState(currentChapter);
-  console.log(links);
   return (
     <>
       <span>
         <button
           onClick={() => setShow(!show)}
           type="button"
-          className={`flex items-center w-full justify-between font-medium text-left text-slate-500
-          ${currentChapter ? "text-green-400" : ""}`}
+          className={`flex items-center w-full justify-between font-medium text-left
+          ${
+            currentChapter
+              ? "text-primary-600 font-bold dark:text-primary-alt-600"
+              : "text-slate-500"
+          }`}
         >
           <AccordionTitle>{title}</AccordionTitle>
-          <IconArrowBadgeUpFilled className={`${show ? "rotate-180" : ""}`} />
+          <IconChevronRight
+            size={size - 4}
+            stroke={stroke}
+            className={`${show ? "rotate-90" : ""}`}
+          />
         </button>
       </span>
       <div className={`${show ? "scale-y-100 " : "hidden"}`}>
-        <div className="p-2 flex items-start justify-start flex-col gap-1">
+        <ol className="p-2 flex items-start justify-start flex-col gap-1">
           {links.map(({ title, slug }, i) => {
             return (
-              <Link
-                className={`flex gap-1 items-center text-white ${
-                  currentLessonIndex === i ? "font-bold" : ""
+              <AccordionLink
+                className={`${
+                  currentLessonIndex === i
+                    ? "font-bold text-slate-900 dark:text-slate-100"
+                    : ""
                 }`}
                 key={i}
-                href={slug}
+                slug={slug}
               >
                 {title}
-              </Link>
+              </AccordionLink>
             );
           })}
-        </div>
+        </ol>
       </div>
     </>
   );

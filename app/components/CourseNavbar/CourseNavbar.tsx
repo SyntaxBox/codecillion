d6791706cdf27e74cb078443e779d9ad9e8017fb
kosei <abdelhamid.boudjit@gmail.com>
2023-10-31 15:03:52 +0100

    Create CourseNavbar.tsx

---
+++ app/components/CourseNavbar/CourseNavbar.tsx
@@ -0,0 +1,38 @@
+import { navbarButtons } from "@/data/elements/navbar";
+import Container from "@/app/UI/layout/Container";
+import React from "react";
+import DarkMode from "../DarkMode/DarkMode";
+import Logo from "../Logo/Logo";
+import NavButtons from "../NavButtons/NavButtons";
+import SlideMenu from "../SlideMenu/SlideMenu";
+import SocialIcons from "../SocialIcons/SocialIcons";
+
+function CourseNavbar() {
+  return (
+    <nav className="backdrop-blur-md bg-[#ffffff8b] dark:bg-[#0f172a8b] z-50 w-full h-[56px] text-gray-700 dark:text-gray-200 relative">
+      <Container className="flex items-center justify-between">
+        <Logo
+          className="hidden lg:flex"
+          width={48}
+          height={48}
+          includeTitle
+          href="/"
+        />
+        <Logo className="lg:hidden" width={48} height={48} href="/" />
+        <NavButtons className="hidden md:flex" buttons={navbarButtons} />
+        <div className="flex items-center gap-6">
+          <SocialIcons className="hidden md:flex" />
+          <DarkMode />
+          <SlideMenu title="The Menu" className="md:hidden">
+            <div className="flex flex-col justify-between">
+              <NavButtons className="flex-col w-full" buttons={navbarButtons} />
+              <SocialIcons className="gap-4 p-2" />
+            </div>
+          </SlideMenu>
+        </div>
+      </Container>
+    </nav>
+  );
+}
+
+export default CourseNavbar;

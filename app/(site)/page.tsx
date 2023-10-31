20e225120eb0647168c817e7cae36c62df32cd5b
kosei <abdelhamid.boudjit@gmail.com>
2023-10-31 15:03:58 +0100

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -14,6 +14,7 @@ import {
   getFeaturedCourse,
   getAllStacks,
 } from "@/sanity/utils";
+import Navbar from "../components/Navbar/Navbar";
 
 export const metadata: Metadata = {
   ...homePageMetadata,
@@ -25,35 +26,41 @@ export default async function Home() {
   const courses = await getAllCourses({ max: 2 });
   const stacks = await getAllStacks({});
   return (
-    <main className="flex flex-col gap-6 items-center md:max-w-[820px] lg:max-w-full mx-auto">
-      <Featured
-        description={featuredCourse.description}
-        href={`/courses/${featuredCourse.slug}`}
-        thumbnail={`${featuredCourse.featured}`}
-        title={featuredCourse.title}
-      />
-      <Courses courses={courses} />
-      <SocialBanner />
-      <Container className="flex flex-col mdlg:flex-row gap-3 lg:gap-6 items-center justify-between">
-        <FlatCard
-          thumbnail={stacks[0].thumbnail}
-          href={`programming-paths${stacks[0].slug}`}
-          title={stacks[0].title}
+    <>
+      <header className="sticky top-0 left-0 z-50">
+        <Navbar />
+      </header>
+
+      <main className="flex flex-col gap-6 items-center md:max-w-[820px] lg:max-w-full mx-auto">
+        <Featured
+          description={featuredCourse.description}
+          href={`/courses/${featuredCourse.slug}`}
+          thumbnail={`${featuredCourse.featured}`}
+          title={featuredCourse.title}
         />
-        <FlatCard
-          thumbnail={stacks[1].thumbnail}
-          href={`programming-paths${stacks[1].slug}`}
-          title={stacks[1].title}
+        <Courses courses={courses} />
+        <SocialBanner />
+        <Container className="flex flex-col mdlg:flex-row gap-3 lg:gap-6 items-center justify-between">
+          <FlatCard
+            thumbnail={stacks[0].thumbnail}
+            href={`programming-paths${stacks[0].slug}`}
+            title={stacks[0].title}
+          />
+          <FlatCard
+            thumbnail={stacks[1].thumbnail}
+            href={`programming-paths${stacks[1].slug}`}
+            title={stacks[1].title}
+          />
+        </Container>
+        <BigCard
+          thumbnail={posts[0].thumbnail}
+          title={posts[0].title}
+          description={posts[0].description}
+          slug={`posts/${posts[0].slug}`}
         />
-      </Container>
-      <BigCard
-        thumbnail={posts[0].thumbnail}
-        title={posts[0].title}
-        description={posts[0].description}
-        slug={`posts/${posts[0].slug}`}
-      />
-      <Posts posts={posts.splice(1, posts.length)} />
-      <ExploreMore href="posts" />
-    </main>
+        <Posts posts={posts.splice(1, posts.length)} />
+        <ExploreMore href="posts" />
+      </main>
+    </>
   );
 }

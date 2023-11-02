836bd11d411da3cc78b50c1d6ff58a5f2987d38e
kosei <abdelhamid.boudjit@gmail.com>
2023-11-02 21:07:16 +0100

    Create SideBar.tsx

---
+++ app/components/CourseSidebar/SideBar.tsx
@@ -0,0 +1,55 @@
+import Accordion from "@/app/UI/Typography/Accordion";
+import { CourseAccordion } from "@/interfaces/queries";
+import React from "react";
+function Sidebar({
+  courseAccordion,
+  currentLesson,
+  className,
+}: {
+  courseAccordion: CourseAccordion[] | null;
+  currentLesson: string;
+  className?: string;
+}) {
+  return (
+    <aside
+      style={{ maxHeight: "calc(100vh - 64px)" }}
+      className={`h-fit overflow-y-scroll overflow-x-hidden auto-hide-scrollbar ${className}`}
+    >
+      {courseAccordion &&
+        courseAccordion.map(({ title, links }, i) => {
+          return (
+            <Accordion
+              title={title}
+              links={links}
+              currentLesson={currentLesson}
+              key={i}
+            />
+          );
+        })}
+      {courseAccordion &&
+        courseAccordion.map(({ title, links }, i) => {
+          return (
+            <Accordion
+              title={title}
+              links={links}
+              currentLesson={currentLesson}
+              key={i}
+            />
+          );
+        })}
+      {courseAccordion &&
+        courseAccordion.map(({ title, links }, i) => {
+          return (
+            <Accordion
+              title={title}
+              links={links}
+              currentLesson={currentLesson}
+              key={i}
+            />
+          );
+        })}
+    </aside>
+  );
+}
+
+export default Sidebar;

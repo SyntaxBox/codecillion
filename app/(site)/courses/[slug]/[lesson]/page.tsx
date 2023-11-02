e0f1a4268d1909301725f5e8f5d278ad5e479e0d
kosei <abdelhamid.boudjit@gmail.com>
2023-11-02 21:54:35 +0100

    Update page.tsx

--- app/(site)/courses/[slug]/[lesson]/page.tsx
+++ app/(site)/courses/[slug]/[lesson]/page.tsx
@@ -14,7 +14,7 @@ export default async function page({ params }: Props) {
   if (!lesson) notFound();
   const { description, title, content } = lesson;
   return (
-    <section className="flex flex-col max-w-3xl mx-auto px-4 gap-3">
+    <section className="flex flex-col max-w-3xl mx-auto gap-3">
       <H1>{title}</H1>
       <div className="backdrop-blur-md w-full bg-slate-100 dark:bg-slate-800 flex flex-col gap-3 p-2 sm:p-4 rounded-lg">
         <p className="capitalize text-sm sm:text-base leading-6  text-slate-700 dark:text-slate-300 tracking-wider">

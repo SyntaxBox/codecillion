a4581362a684a39a755fcb455478e791cc33cd8f
kosei <abdelhamid.boudjit@gmail.com>
2023-11-03 16:14:47 +0100

    Update CurrentLesson.tsx

--- app/components/CourseSidebar/CurrentLesson.tsx
+++ app/components/CourseSidebar/CurrentLesson.tsx
@@ -1,27 +1,44 @@
 "use client";
+import { size, stroke } from "@/constants/elements/icons";
 import { CourseAccordion, CourseAccordionLink } from "@/interfaces/queries";
 import { findSlugIndex } from "@/logic/accordion";
+import { IconChevronDown } from "@tabler/icons-react";
 import React, { MouseEventHandler, useEffect, useState } from "react";
 
 function CurrentLesson({
   currentLesson,
   courseAccordion,
   onClick,
+  rotateChevron = false,
+  className,
 }: {
   currentLesson: string;
   courseAccordion: CourseAccordion[] | null;
   onClick?: MouseEventHandler<HTMLButtonElement>;
+  rotateChevron?: boolean;
+  className?: string;
 }) {
   const [links, setLinks] = useState<CourseAccordionLink[]>([]);
   useEffect(() => {
-    courseAccordion?.map((e) => setLinks((links) => [...links, ...e.links]));
-  });
+    const tempLinks: CourseAccordionLink[] = [];
+    courseAccordion?.map((e) => tempLinks.push(...e.links));
+    tempLinks && setLinks(tempLinks);
+  }, [courseAccordion]);
 
-  const currentLessonIndex = findSlugIndex(links, currentLesson);
-  const currentLessonTitle = links[currentLessonIndex]?.title;
+  const currentLessonTitle = links[findSlugIndex(links, currentLesson)]?.title;
   return (
-    <button className={`bg-inherit p-2`} onClick={onClick}>
-      {currentLessonTitle}
+    <button
+      className={`bg-white dark:bg-slate-900 z-50 p-2 flex items-center justify-between w-full sticky top-14 -mt-3 ${className}`}
+      onClick={onClick}
+    >
+      <p className="text-start font-bold text-lg line-clamp-1 text-ellipsis overflow-hidden">
+        {currentLessonTitle}
+      </p>
+      <IconChevronDown
+        size={size - 4}
+        stroke={stroke}
+        className={`${rotateChevron ? "rotate-180" : ""}`}
+      />
     </button>
   );
 }

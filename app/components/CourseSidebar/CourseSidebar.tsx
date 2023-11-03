3be29eed3cb0ebb767d3ac04837344973af531fc
kosei <abdelhamid.boudjit@gmail.com>
2023-11-03 16:14:49 +0100

    Update CourseSidebar.tsx

--- app/components/CourseSidebar/CourseSidebar.tsx
+++ app/components/CourseSidebar/CourseSidebar.tsx
@@ -11,36 +11,50 @@ import CurrentLesson from "./CurrentLesson";
 function CourseSidebar() {
   const params = useParams();
   const [course, setCourse] = useState<CourseContentMap[] | null>(null);
-  const [showScrollbar, setShowScrollbar] = useState(false);
+  const [showSidebarOnSmallScreen, setShowSidebarOnSmallScreen] =
+    useState(false);
   const [courseAccordion, setCourseAccordion] = useState<
     CourseAccordion[] | null
   >(null);
-  const handleCourseContent = async () => {
-    if (!params?.slug || !params?.lesson) console.log("fatal at CourseSidebar");
-    else {
-      const courseContent = await getCourseContentBySlug(
-        Array.isArray(params.slug) ? params.slug[0] : params.slug
-      );
-      setCourse(courseContent.content);
-    }
-  };
+
   useEffect(() => {
-    handleCourseContent();
-  }, [params]);
+    const fetchData = async () => {
+      if (params?.slug) {
+        try {
+          const courseContent = await getCourseContentBySlug(
+            Array.isArray(params.slug) ? params.slug[0] : params.slug
+          );
+          setCourse(courseContent.content);
+        } catch (error) {
+          // Handle any errors here
+          console.error("Error fetching course content:", error);
+        }
+      }
+    };
+
+    fetchData();
+  }, [params?.slug]);
+
   useEffect(() => {
     if (course) setCourseAccordion(transformToAccordion(course));
   }, [course]);
+
   if (!params?.slug || !params?.lesson) return null;
   return (
     <>
       <CurrentLesson
+        className="md:hidden"
         courseAccordion={courseAccordion}
         currentLesson={
           Array.isArray(params.lesson) ? params.lesson[0] : params.lesson
         }
+        onClick={() => setShowSidebarOnSmallScreen((v) => !v)}
+        rotateChevron={showSidebarOnSmallScreen}
       />
       <Sidebar
-        className="w-72 min-w-[200px] top-[66px] sticky hidden md:block"
+        className={`w-full pr-8 pl-2 pb-12 top-24 md:w-72 md:min-w-[200px] md:top-[66px] fixed md:sticky md:p-0 ${
+          showSidebarOnSmallScreen ? "block" : "hidden md:block"
+        }`}
         courseAccordion={courseAccordion}
         currentLesson={
           Array.isArray(params.lesson) ? params.lesson[0] : params.lesson

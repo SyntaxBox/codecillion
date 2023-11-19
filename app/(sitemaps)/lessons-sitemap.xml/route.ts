9c70c4bd8b1b1cbf5b141d961412870717712f0c
kosei <abdelhamid.boudjit@gmail.com>
2023-11-19 21:24:41 +0100

    Create route.ts

---
+++ app/(sitemaps)/lessons-sitemap.xml/route.ts
@@ -0,0 +1,18 @@
+// app/server-sitemap.xml/route.ts
+import { URL } from "@/constants/other";
+import { lessonsSitemap } from "@/sanity/utils";
+import { getServerSideSitemap } from "next-sitemap";
+
+export async function GET(request: Request) {
+  const posts = await lessonsSitemap();
+  return getServerSideSitemap(
+    posts.map(({ slug, updatedAt }) => {
+      return {
+        loc: `${URL}/posts/${slug}`,
+        lastmod: new Date(updatedAt).toISOString(),
+        changefreq: "daily",
+        priority: 0.7,
+      };
+    })
+  );
+}

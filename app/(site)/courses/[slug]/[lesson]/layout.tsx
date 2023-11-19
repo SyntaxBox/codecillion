5e290b1e1f0b5a70e28d7641aee2924fd0026697
kosei <abdelhamid.boudjit@gmail.com>
2023-11-19 21:24:51 +0100

    Update layout.tsx

--- app/(site)/courses/[slug]/[lesson]/layout.tsx
+++ app/(site)/courses/[slug]/[lesson]/layout.tsx
@@ -1,15 +1,15 @@
-// import { TWITTER } from "@/constants/other";
-// import {
-//   twitterMetadata,
-//   openGraphMetadata,
-//   themeColorMetadata,
-//   manifestMetadata,
-//   iconsMetadata,
-// } from "@/data/meta/global";
-// import { getCourseMetadataBySlug } from "@/sanity/utils";
-// import { Metadata } from "next";
-// import { metadata as dynamicMetadata } from "@/data/meta/pages/course";
-// import { URL } from "@/constants/other";
+import { TWITTER } from "@/constants/other";
+import {
+  twitterMetadata,
+  openGraphMetadata,
+  themeColorMetadata,
+  manifestMetadata,
+  iconsMetadata,
+} from "@/data/meta/global";
+import { getCourseMetadataBySlug } from "@/sanity/utils";
+import { Metadata } from "next";
+import { metadata as dynamicMetadata } from "@/data/meta/pages/course";
+import { URL } from "@/constants/other";
 
 import Container from "@/app/UI/layout/Container";
 import CourseSidebar from "@/app/components/CourseSidebar/CourseSidebar";
@@ -21,30 +21,30 @@ type Props = {
   router: any;
 };
 
-// export async function generateMetadata({ params }: Props): Promise<Metadata> {
-//   // read route params
-//   const { slug } = params;
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // read route params
+  const { lesson } = params;
 
-//   // fetch data
-//   const courseMetadata = await getCourseMetadataBySlug(slug);
-//   if (courseMetadata?.keywords === null) courseMetadata.keywords = [];
-//   if (courseMetadata) {
-//     const { title, description, thumbnail } = courseMetadata;
-//     const metadata: Metadata = {
-//       ...dynamicMetadata(courseMetadata),
-//       ...twitterMetadata(title, description, thumbnail, TWITTER),
-//       ...openGraphMetadata(title, description, thumbnail, `${URL}/${slug}`),
-//       ...themeColorMetadata,
-//       ...manifestMetadata,
-//       ...iconsMetadata,
-//     };
-//     return metadata;
-//   } else {
-//     return {
-//       title: "Not Found",
-//     };
-//   }
-// }
+  // fetch data
+  const courseMetadata = await getCourseMetadataBySlug(slug);
+  if (courseMetadata?.keywords === null) courseMetadata.keywords = [];
+  if (courseMetadata) {
+    const { title, description, thumbnail } = courseMetadata;
+    const metadata: Metadata = {
+      ...dynamicMetadata(courseMetadata),
+      ...twitterMetadata(title, description, thumbnail, TWITTER),
+      ...openGraphMetadata(title, description, thumbnail, `${URL}/${slug}`),
+      ...themeColorMetadata,
+      ...manifestMetadata,
+      ...iconsMetadata,
+    };
+    return metadata;
+  } else {
+    return {
+      title: "Not Found",
+    };
+  }
+}
 
 export default async function Layout({ children }: { children: ReactNode }) {
   return (

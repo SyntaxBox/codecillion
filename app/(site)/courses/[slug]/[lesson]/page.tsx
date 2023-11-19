2f90de5da76f9caaf42b05777feed0f5b5205ca2
kosei <abdelhamid.boudjit@gmail.com>
2023-11-19 21:24:48 +0100

    Update page.tsx

--- app/(site)/courses/[slug]/[lesson]/page.tsx
+++ app/(site)/courses/[slug]/[lesson]/page.tsx
@@ -1,6 +1,6 @@
 import H1 from "@/app/UI/Typography/H1";
 import PostContent from "@/app/components/PostContent/PostContent";
-import { getLessonBySlug } from "@/sanity/utils/lesson";
+import { getLessonBySlug } from "@/sanity/utils";
 import { notFound } from "next/navigation";
 import React from "react";
 

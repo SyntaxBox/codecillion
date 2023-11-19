574cbc7620346d1201ba20d673e76ea30dbe6357
kosei <abdelhamid.boudjit@gmail.com>
2023-11-19 21:24:27 +0100

    Update index.ts

--- sanity/utils/index.ts
+++ sanity/utils/index.ts
@@ -1,6 +1,7 @@
 export * from "./courses";
 export * from "./posts";
 export * from "./stacks";
+export * from "./lesson";
 import { createClient } from "next-sanity";
 import { clientConfig } from "@/config/sanity.client";
 export const client = createClient(clientConfig);

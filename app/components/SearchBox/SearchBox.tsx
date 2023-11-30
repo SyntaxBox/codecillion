e32d39cf2180bbe896eb830fb0a23d4681e93b27
kosei <abdelhamid.boudjit@gmail.com>
2023-11-30 17:49:01 +0100

    Update SearchBox.tsx

--- app/components/SearchBox/SearchBox.tsx
+++ app/components/SearchBox/SearchBox.tsx
@@ -1,8 +1,6 @@
-import { size, stroke } from "@/constants/elements/icons";
+import { iconSize, iconStroke } from "@/constants";
 import { IconSearch } from "@tabler/icons-react";
-import React, { ChangeEventHandler } from "react";
-
-type props = {} & JSX.IntrinsicElements["input"];
+import React from "react";
 
 function SearchBox({ className, ...rest }: JSX.IntrinsicElements["input"]) {
   return (
@@ -13,8 +11,8 @@ function SearchBox({ className, ...rest }: JSX.IntrinsicElements["input"]) {
         className={`w-full pl-10 pr-4 py-2 bg-slate-200 dark:bg-slate-700  rounded-lg shadow-2xl shadow-slate-200 dark:shadow-slate-700 focus:outline-none focus:ring-2 focus:ring-primary-500 dark:ring-primary-alt-500 placeholder:text-gray-400 dark:placeholder:text-gray-300 ${className}`}
       />
       <IconSearch
-        size={size}
-        stroke={stroke}
+        size={iconSize}
+        stroke={iconStroke}
         className="absolute left-3 top-2.5 h-5 w-5 text-gray-400 dark:text-gray-300"
       />
     </div>

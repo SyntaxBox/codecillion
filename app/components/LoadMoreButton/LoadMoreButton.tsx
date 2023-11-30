b199641a014ba2f490f211089c312b2bf726c9d9
kosei <abdelhamid.boudjit@gmail.com>
2023-11-30 17:49:31 +0100

    Update LoadMoreButton.tsx

--- app/components/LoadMoreButton/LoadMoreButton.tsx
+++ app/components/LoadMoreButton/LoadMoreButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { IconDots, IconFidgetSpinner } from "@tabler/icons-react";
-import React, { useState } from "react";
+import React from "react";
 
 type props = {
   text: string;

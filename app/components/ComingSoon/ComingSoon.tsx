9920764c259f36f7c23f811e0aa79d4996bb24ee
kosei <abdelhamid.boudjit@gmail.com>
2023-12-11 16:39:56 +0100

    Update ComingSoon.tsx

--- app/components/ComingSoon/ComingSoon.tsx
+++ app/components/ComingSoon/ComingSoon.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { Container } from "@/app/UI";
-import { A } from "@/app/UI/typography/A";
+import { Container, A } from "@/app/UI";
 import React from "react";
 
 function ComingSoon({ description }: { description: string }) {

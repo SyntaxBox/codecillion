8f4f32d24d572f461bf8aeea19acf0d925d70621
kosei <abdelhamid.boudjit@gmail.com>
2023-12-17 18:11:26 +0100

    Update layout.tsx

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,12 +2,25 @@ import Providers from "./Providers";
 import "../globals.css";
 import { Barlow } from "next/font/google";
 import Footer from "../components/Footer/Footer";
+import { Viewport } from "next";
 
 const font = Barlow({
   weight: ["400", "700", "300"],
   subsets: ["latin"],
 });
 
+export const viewport: Viewport = {
+  themeColor: [
+    {
+      color: "#fff",
+      media: "(prefers-color-scheme: light)",
+    },
+    {
+      color: "#0f172a",
+      media: "(prefers-color-scheme: dark)",
+    },
+  ],
+};
 export default function RootLayout({
   children,
 }: {

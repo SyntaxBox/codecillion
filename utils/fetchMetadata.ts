a01a9c854a8bee7b681c4c20b8ed547e4843941c
kosei <abdelhamid.boudjit@gmail.com>
2023-12-17 18:10:45 +0100

    Update fetchMetadata.ts

--- utils/fetchMetadata.ts
+++ utils/fetchMetadata.ts
@@ -1,23 +1,23 @@
-// utils/fetchWebsiteMetadata.js
-import fetch from "node-fetch";
-import cheerio from "cheerio";
+import axios from "axios";
+import * as cheerio from "cheerio";
 
 export async function fetchWebsiteMetadata(
   url: string
 ): Promise<{ title: string } | null> {
   try {
-    // Fetch the HTML content of the website
-    // const response = await fetch(url);
-    // const html = await response.text();
+    console.log(url);
+    // Fetch the HTML content of the website using axios
+    const response = await axios.get(url);
+    const html = response.data;
 
-    // // Use cheerio to parse the HTML
-    // const $ = cheerio.load(html);
+    // Use cheerio to parse the HTML
+    const $ = cheerio.load(html);
 
-    // // Extract the title and description
-    // const title = $("head title").text();
-    // // const description = $('meta[name="description"]').attr("content");
+    // Extract the title and description
+    const title = $("head title").text();
+    // const description = $('meta[name="description"]').attr("content");
 
-    return { title: "hello qo" };
+    return { title };
   } catch (error) {
     console.error("Error fetching website metadata:", error);
     return null;

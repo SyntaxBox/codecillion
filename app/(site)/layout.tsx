9356a618783464ae698b262320bf88899ae1c1dc
kosei <abdelhamid.boudjit@gmail.com>
2023-12-19 14:30:52 +0100

    Update layout.tsx

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -2,12 +2,36 @@ import Providers from "./Providers";
 import "../globals.css";
 import { Barlow } from "next/font/google";
 import Footer from "../components/Footer/Footer";
+<<<<<<< Updated upstream
+=======
+import { Viewport } from "next";
+import { metadataBase } from "@/data";
+>>>>>>> Stashed changes
 
 const font = Barlow({
   weight: ["400", "700", "300"],
   subsets: ["latin"],
 });
 
+<<<<<<< Updated upstream
+=======
+export const viewport: Viewport = {
+  themeColor: [
+    {
+      color: "#fff",
+      media: "(prefers-color-scheme: light)",
+    },
+    {
+      color: "#0f172a",
+      media: "(prefers-color-scheme: dark)",
+    },
+  ],
+};
+
+export const metadata = {
+  metadataBase,
+};
+>>>>>>> Stashed changes
 export default function RootLayout({
   children,
 }: {

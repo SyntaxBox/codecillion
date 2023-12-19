aec4e08a00f5a5105fb887ccfa197624783e5647
kosei <abdelhamid.boudjit@gmail.com>
2023-12-19 14:30:36 +0100

    Update index.ts

--- data/meta/global/index.ts
+++ data/meta/global/index.ts
@@ -2,4 +2,8 @@ export * from "./icons";
 export * from "./manifest";
 export * from "./openGraph";
 export * from "./twitter";
+<<<<<<< Updated upstream
 export * from "./themeColor";
+=======
+export * from "./metadataBase";
+>>>>>>> Stashed changes
